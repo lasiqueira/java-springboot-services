90556bcaa2275143b8b68dfaaa661f3105e6f49f
Lucas Siqueira <lucas.asiq@gmail.com>
2018-12-08 22:49:44 -0200

    added metrics

--- src/main/java/br/unisinos/apps4business/biometrics/message/listener/BiometricsListener.java
+++ src/main/java/br/unisinos/apps4business/biometrics/message/listener/BiometricsListener.java
@@ -3,6 +3,8 @@ package br.unisinos.apps4business.biometrics.message.listener;
 import br.unisinos.apps4business.biometrics.api.v1.converter.BiometricsConverter;
 import br.unisinos.apps4business.biometrics.message.dto.BiometricsMessageDTO;
 import br.unisinos.apps4business.biometrics.service.BiometricsService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +12,17 @@ import org.springframework.stereotype.Service;
 public class BiometricsListener {
     private BiometricsService biometricsService;
     private BiometricsConverter biometricsConverter;
+    private Counter counter;
 
-    public BiometricsListener(BiometricsService biometricsService, BiometricsConverter biometricsConverter) {
+    public BiometricsListener(BiometricsService biometricsService, BiometricsConverter biometricsConverter, MeterRegistry meterRegistry) {
         this.biometricsService = biometricsService;
         this.biometricsConverter = biometricsConverter;
+        this.counter = meterRegistry.counter("received.messages");
     }
 
     @RabbitListener(queues = "queue")
     public void handle(BiometricsMessageDTO biometricsMessageDTO){
+        counter.increment();
         biometricsService.saveBiometrics(biometricsConverter.convertMessageDTOToEntity(biometricsMessageDTO));
     }
 }
--- src/main/java/br/unisinos/apps4business/biometrics/message/producer/BiometricsProducer.java
+++ src/main/java/br/unisinos/apps4business/biometrics/message/producer/BiometricsProducer.java
@@ -1,6 +1,8 @@
 package br.unisinos.apps4business.biometrics.message.producer;
 
 import br.unisinos.apps4business.biometrics.message.dto.BiometricsMessageDTO;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -9,14 +11,16 @@ import org.springframework.stereotype.Service;
 public class BiometricsProducer {
     private RabbitTemplate rabbitTemplate;
     private Queue queue;
+    private Counter counter;
 
-    public BiometricsProducer(RabbitTemplate rabbitTemplate, Queue queue) {
+    public BiometricsProducer(RabbitTemplate rabbitTemplate, Queue queue, MeterRegistry meterRegistry) {
         this.rabbitTemplate = rabbitTemplate;
         this.queue = queue;
+        this.counter = meterRegistry.counter("sent.messages");
     }
 
     public void sendMessage(BiometricsMessageDTO biometricsMessageDTO){
-
+        counter.increment();
         rabbitTemplate.convertAndSend(queue.getName(),biometricsMessageDTO);
     }
 }
